d8f684c6ec37e9e91171d5e8d88d5cda7c1bad1c
pdtowctor <pdtowctor@gmail.com>
2022-11-16 11:19:28 +0900

    Update FIBO.sv [rst -> rst_n]

--- DMAC/RTL/FIBO.sv
+++ DMAC/RTL/FIBO.sv
@@ -1,7 +1,7 @@
 module FIBO
 (
     input   wire            clk,
-    input   wire            rst,
+    input   wire            rst_n,
     input   wire            enable,
     output  wire [31:0]     sum
 );
@@ -9,14 +9,14 @@ module FIBO
 reg [31:0] cur_num;
 reg [31:0] next_num;
 
-always @(posedge clk or negedge rst)
-    if (!rst)
+always @(posedge clk or negedge rst_n)
+    if (!rst_n)
         cur_num <= #1 32'b0;    // initialize to 0
     else if (enable)
         cur_num <= next_num;
 
-always @(posedge clk or negedge rst)
-    if (!rst)
+always @(posedge clk or negedge rst_n)
+    if (!rst_n)
         next_num    <= 32'b1;
     else if (enable)
         next_num    <= sum;
